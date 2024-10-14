9e960322f6018dcab8c3a94cf4ccae1f67bf9139
Boyi Qian <boyiq@andrew.cmu.edu>
2024-10-14 02:19:35 -0400

    add update single transcript to KG, still working on ASR integrate

--- upstreamPipeline/upstream_pipeline.py
+++ upstreamPipeline/upstream_pipeline.py
@@ -1,5 +1,6 @@
 from neo4j_processor import Neo4jProcessor
 from file_processor import FileProcessor
+
 import argparse
 import os
 from configparser import ConfigParser
@@ -35,20 +36,35 @@ if __name__ == "__main__":
     parser.add_argument("--save-dir", type=str, required=False, help="Directory to save processed files.")
     parser.add_argument("--filename", type=str, required=False, help="Name of a specific file to process.")
     parser.add_argument("--generate-from-rar", action="store_true", help="Flag to indicate if generate directly based on the xml.rar we provide")
+    parser.add_augument("--add-stock",action="store_true", help="Flag to indicate if user want to use ASR to add timeStamp and minute level stock price. If so, please add audio folder and audio name")
+    parser.add_argument("--audio-dir", type=str, required=False, help="Directory containing the audio file.")
+    parser.add_argument("--audio-file", type=str, required=False, help="Name of a specific audio file to process.")
+    parser.add_argument("--stock-dir", type=str, required=False, help="Directory to save or read stock information.")
+    parser.add_argument("--has-stock-data", action="store_true", help="Flag to indicate if we already have minute level stock data")
+    parser.add_argument("--clean_db", action="store_true", help="add the flag to clean the current database")
     args = parser.parse_args()
 
+    #process_file(self, audio_path,audio_file, stock_folder, xml_path, xml_file, has_stock_data):
+    #"recording", "The Bank of New York Mellon Corporation (NYSE_BK) Jul-12-2024 - Audio.mp3", "stock", "xml", "BK-Q1-2024.xml", False
     processor = FileProcessor(file_dir=args.file_dir, save_dir=args.save_dir, filename=args.filename)
     if args.generate_from_rar:
         print(args.generate_from_rar)
         neo4j_import_folder(args.file_dir)
+    
     if args.filename:
+        print(f"processing single file: {args.filename}")
         file_name = args.filename
-        # file_name = processor.process_single_file()
-        # print(f"neo 4j processing {file_name} on {AUTH}")
+        file_name = processor.process_single_file()
+
+        # if args.add_stock:
+        #     print(f"Generate time stamp for file: {file_name}")
+        #     stock_processor = TimeStampStockProcessor()
+        #     stock_processor.process_file(args.audio_dir, args.audio_file,  args.stock_dir, args.save_dir, file_name, False)
+        print(f"neo 4j processing {file_name} on {AUTH}")
         neo4j_import_single_file(file_name)
-        print("here")
+
     else:
-        # processor.process_all_files()
+        processor.process_all_files()
         neo4j_import_folder(args.save_dir)
 
 '''
@@ -56,7 +72,7 @@ Example:
 
 python upstream_pipeline.py --save-dir "xml" --generate-from-rar
 
-python upstream_pipeline.py --file-dir "transcripts/BK" --save-dir "xml" --filename "The Bank of New York Mellon Corporation, Q1 2024 Earnings Call, Apr 16, 2024.rtf"
+python upstream_pipeline.py --file-dir "transcripts/BK" --save-dir "xml" --filename "The Bank of New York Mellon Corporation, Q2 2024 Earnings Call, Jul 12, 2024.rtf"
 python upstream_pipeline.py --file-dir "transcripts" --save-dir "xml"
 python upstream_pipeline.py --file-dir "xml" --save-dir "xml" --filename "xml\STT-Q1-2024_timestamp.xml"
 
